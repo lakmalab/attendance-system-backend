eca415cc87b0718f116e058c0eb05130b7d3eb7f
lakma <lakmalaberathne@gmail.com>
2025-08-31 09:37:49 +0530

    initial commit

---
+++ src/main/java/model/dto/UserDto.java
@@ -0,0 +1,18 @@
+package model.dto;
+
+public class UserDto {
+
+    private long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+
+
+    private Long projectId;
+
+    private String projectName;
+}
